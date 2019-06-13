e8f4895bf740076ae0e4cab62b83e99ce5cc4427
johannacamara <50372278+johannacamara@users.noreply.github.com>
2019-06-13 19:29:06 -0300

    Add files via upload

---
+++ ShooterNewQuintaParte/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1560464863,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowupbotton-sheet0.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/backgroundanimation-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/botondstroy-sheet0.png",
+		"images/energyui-sheet0.png",
+		"images/energyframeui-sheet0.png",
+		"images/powerupcontroller-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/powerupcontroller2-sheet0.png",
+		"images/parede-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
